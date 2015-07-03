461a941fa63e2daa1e0a4fc99c12bd830cd293be
Joanna Bartolome <betterwings22@gmail.com>
2015-07-03 13:38:25 -0700

    Initial commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Joanna Bartolome on 7/3/15.
+//  Copyright (c) 2015 Joanna Bartolome. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
